8e8bee6519d05b60a2092fc22af8a153b2e2d25d
Chilembwe Asante <cdasante@knox.edu>
2022-06-07 09:45:31 -0500

    Numbers

--- Fibonacci.py
+++ Fibonacci.py
@@ -20,9 +20,9 @@ if nterms <= 0:
     else:
         print("Fibonacci sequence:")
             while count < nterms:
-       print(n1)
-       nth = n1 + n2
-       # update values
-       n1 = n2
-       n2 = nth
-       count += 1
+                print(n1)
+                nth = n1 + n2
+# Update values
+                n1 = n2
+                n2 = nth
+                count += 1
